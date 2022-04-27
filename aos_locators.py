475b1d79286cc10585d2bec3e6780e2c842bd5c8
tttotoro <cathy.yuc@gmail.com>
2022-04-26 22:01:37 -0700

    comment about the update

--- aos_locators.py
+++ aos_locators.py
@@ -5,7 +5,10 @@ aos_url = 'https://advantageonlineshopping.com/#/'
 aos_register_url = 'https://advantageonlineshopping.com/#/register'
 facebook_url = 'https://www.facebook.com/MicroFocus'
 twitter_url = 'https://twitter.com/MicroFocus'
-linkedin_url = 'https://www.linkedin.com/authwall?trk=qf&originalReferer=https://advantageonlineshopping.com/&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2F1024%3Ftrk%3Dtyah%26trkInfo%3DclickedVertical%253Ashowcase%252CclickedEntityId%253A1024%252Cidx%253A2-1-2%252CtarId%253A145431482.327%252Ctas%253Ahewlett%2520packard%2520enterprise%2520software'
+linkedin_url = 'https://www.linkedin.com/authwall?trk=qf&originalReferer=https://advantageonlineshopping.com/' \
+               '&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2F1024%3Ftrk%3Dtyah%26trkInfo%' \
+               '3DclickedVertical%253Ashowcase%252CclickedEntityId%253A1024%252Cidx%253A2-1-2%252Ctar' \
+               'Id%253A145431482.327%252Ctas%253Ahewlett%2520packard%2520enterprise%2520software'
 aos_orders_url = 'https://advantageonlineshopping.com/#/MyOrders'
 aos_order_payment_url = 'https://advantageonlineshopping.com/#/orderPayment'
 aos_title = 'Advantage Shopping'
@@ -20,9 +23,8 @@ full_name = f'{first_name} {last_name}'  # f''    f string
 phone_number = fake.phone_number()[5:10]
 city = fake.city()
 address = fake.street_address()
-# address0 = fake.address().replace('\n', '')  # ask Mr.Singh
-# address2 = f'{fake.street_address}'
-address1 = f'{fake.street_address()}, {city}, {fake.province_abbr()}, {fake.current_country()} {fake.postalcode_in_province()}'
+address1 = f'{fake.street_address()}, {city}, {fake.province_abbr()}, ' \
+           f'{fake.current_country()} {fake.postalcode_in_province()}'
 state_province_region = fake.province_abbr()
 postal_code = fake.postalcode_in_province()  # from https://faker.readthedocs.io/en/master/locales/en_CA.html
 safepay_username = fake.user_name()
@@ -30,7 +32,6 @@ safepay_password = fake.password()
 tracking_number = ""
 order_number = ""
 order_element = ""
-
 
 
 # to take a screenshot while running the test
